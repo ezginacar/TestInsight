c5e912bddcdfbd537f2a377a8df210015a2d0197
Ezgi Nacar <ncrezgi@gmail.com>
2026-03-19 22:10:47 +0300

    feat(auth): refactor auth tests and add new test cases feat(zeta): add initial implementation of zeta API tests

---
+++ tests/api/zeta.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+
+  test('Api zeta tests 01', async ({ request }) => {
+    await console.log('beta test module 01');
+  });
+
+  test('Api zetatests 02', async ({ request }) => {
+    expect(false).toBeTruthy();
+  });
+
+
+
+
+  
